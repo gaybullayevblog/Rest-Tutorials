93bb645d30de057537b699661e0bcfed53216f2a
gaybullayevblog <gaybullayevblog@gmail.com>
2023-09-18 08:30:11 +0500

    done

--- todo_project/frontend/src/App.js
+++ todo_project/frontend/src/App.js
@@ -1,45 +1,36 @@
 import React, { Component } from 'react'
 import './App.css';
-
-const list = [
-  {
-    "id": 1,
-    "title": "Men bugun django darslarini ko'rishim kk",
-    "body": "Django darslari men uchun juda muhim hisoblanadi"
-  },
-  {
-    "id": 2,
-    "title": "Bugun Diqqat kitobini o'qib chiqishim kerak",
-    "body": "Diqqat kitobi menga judayam yoqadi"
-  },
-  {
-    "id": 3,
-    "title": "Bu shunchaki sarlavha",
-    "body": "Bu shunchaki sarlavhaga izoh"
-  },
-   {
-        "id": 4,
-        "title": "Sarlavha",
-        "body": "Bu shunchaki matn"
-   }
-]
+import axios from 'axios'
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state={ list };
+  state = {
+    todos:[]
+  };
+  //yangi
+  componentDidMount(){
+    this.getTodos();
+  }
+  getTodos(){
+    axios
+    .get('http://127.0.0.1:8000/api/')
+    .then(res=>{
+      this.setState({todos:res.data});
+    })
+    .catch(err=>{
+      console.log(err);
+    })
   }
   render(){
     return(
       <div>
-        {this.state.list.map(item=>(
+        {this.state.todos.map(item=>(
           <div key={item.id}>
             <h1>{item.title}</h1>
-            <p>{item.body}</p>
+            <span>{item.body}</span>
           </div>
         ))}
       </div>
-    );
+    )
   }
 }
 
